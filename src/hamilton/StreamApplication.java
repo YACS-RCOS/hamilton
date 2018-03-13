15b6a681fca84fc1ececdea1828746a24ce81b50
Yuze.Ma <584653629@qq.com>
2018-03-13 18:30:28 -0400

    Fix the project setup

--- src/hamilton/StreamApplication.java
+++
@@ -1,5 +0,0 @@
-package hamilton;
-
-public class StreamApplication {
-
-}
---
+++ src/main/java/yacs_mq/hamilton/App.java
@@ -0,0 +1,13 @@
+package yacs_mq.hamilton;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello hamilton" );
+    }
+}
